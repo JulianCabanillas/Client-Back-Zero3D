5624f9438931378555f51f7166e9f2af81378a43
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-06-07 20:32:32 +0200

    [UPD]Cookies Upd

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path # type: ignore
 from . import views
 
+
 # Aqui derivamos las peticiones entrantes a la API a su vista correspondiente:
 urlpatterns = [
-    path('register/', views.register_client, name='register_client'),
+    path('register_client/', views.register_client, name='register_client'),
     path('clients/', views.get_clients, name='get_clients'),
     path('clients/<int:pk>/', views.get_client_by_id, name='get_client_by_id'),
-    path('login/', views.login_client, name='login_client'),
+    path('login_client/', views.login_client, name='login_client'),
     path('calculate/', views.calculate_budget, name='calculate_budget'),
+    path('token/refresh/', views.refresh_access_token),
 ]
