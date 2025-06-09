8b9ef20c4307357a780c9ea46c87b5c03c4a12e1
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-06-09 16:15:10 +0200

    [FIX]Fixed ORDERS  -v0.8.1A

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Modelo de los tecnicos:
+# Modelo de los tecnicos (Proximamente, futuros upds):
 class Technical(models.Model):
 	technical_id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=100)
@@ -12,6 +12,11 @@ class Technical(models.Model):
 
 # Modelo de los clientes:
 class Client(models.Model):
+    # Utilizamos esta propiedad para poder validar el usuario 
+    # cuando realiza el pedido:
+    @property
+    def is_authenticated(self):   
+        return True
     client_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
@@ -19,6 +24,7 @@ class Client(models.Model):
     email = models.EmailField(unique=True)
     date_register = models.DateField()
 
+    
     def __str__(self):
         return f"Client {self.client_id}"
 
@@ -28,13 +34,16 @@ class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='orders')
     technical = models.ForeignKey(Technical, on_delete=models.SET_NULL, null=True, related_name='orders')
     total_euros = models.FloatField()
-    date_register = models.DateTimeField(auto_now_add=True)          # mejor que CharField
-    stl_file      = models.FileField(upload_to="orders/%Y/%m/%d/", null=True,  blank=True,)
+    date_register = models.DateTimeField(auto_now_add=True)   
+    stl_file = models.FileField(upload_to="orders/%Y/%m/%d/", null=True,  blank=True,)
+    material = models.CharField(max_length=100, default='pla')
+    velocity = models.DecimalField(decimal_places=0, max_digits=2, default=30)
+    color = models.CharField(max_length=100, default='blanco')
     def __str__(self):
         return f"Order {self.order_id}"
 
 
-# Modelo de las impresoras:
+# Modelo de las impresoras (Proximamente, futuros upds):
 class Printer(models.Model):
     printer_id = models.AutoField(primary_key=True)
     description = models.TextField()
--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers # type: ignore
 from .models import Client, Order
 
+# Estos serializadores nos ayudarana en la traduccion de los datos con la 
+# base de datos mediante conversiones a JSON y viceversa.
+
 # Serializador para los datos del cliente:
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['client_id','username', 'password', 'direction', 'email', 'date_register']
+        # Nos aseguramos de que la contraseña solo se envie, nunca se 
+        # mande a una peticion de usuario (Seguridad):
         extra_kwargs = {             
             'password': {'write_only': True}
         }
@@ -15,5 +20,8 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Order
         fields = ["order_id", "client", "technical",
-                  "total_euros", "date_register", "stl_file"]
-        read_only_fields = ["order_id", "technical", "date_register"]
+                  "total_euros", "date_register", "stl_file", "material", "velocity", "color"]
+        # Aqui tenemos las columnas que daremos valor una vez instaciados, utilizamos read_only_field
+        # para que no gerenere fallos en el inicio (no sive estos datos en JSON) y darle valor 
+        # a posterior en la vista:
+        read_only_fields = ["order_id", "client", "technical", "date_register"]
