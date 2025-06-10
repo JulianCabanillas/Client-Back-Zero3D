b20aae6a2a99d46b5d4b87f27b7dfb0be8901f47
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-06-10 21:58:24 +0200

    a

--- appBackClient/settings/base.py
+++ appBackClient/settings/base.py
@@ -75,6 +75,9 @@ REST_FRAMEWORK = {
       'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         ),
+        'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 #Tiempos que queremos mantener la validacion de dichas cokies y otros 
